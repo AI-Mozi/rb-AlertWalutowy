51110370d691a60537f831a694daf332b85cc688
AI-Mozi <bieniek.milosz0@gmail.com>
2020-12-22 17:10:46 +0100

    Basic frontend

--- app/controllers/user_currency_controller.rb
+++ app/controllers/user_currency_controller.rb
@@ -1,17 +1,24 @@
 class UserCurrencyController < ApplicationController
+  before_action :require_login
+
+  def new
+     @user_currency = UserCurrency.new()
+  end
   
-    def new
-      @user_currency = UserCurrency.new()
-    end
+  def create
+    UserCurrencyManager.new(params[:user][:currency_ids], current_user).call
+    redirect_to logged_user_index_path
+  end
   
-    def create
-      UserCurrencyManager.new(params[:user][:currency_ids], current_user).call
-      redirect_to logged_user_index_path
-    end
+  private
+
+  def user_currency_params
+    params.permit(:user_id,  currency_id:[])
+  end
   
-    private
-    def user_currency_params
-      params.permit(:user_id,  currency_id:[])
+  def require_login
+    unless current_user
+      redirect_to new_user_session_url
     end
-   
+  end
 end
