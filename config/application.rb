8a019f850c9c09c9fe62cd158e552d33f009836e
AI-Mozi <bieniek.milosz0@gmail.com>
2021-02-19 10:52:27 +0100

    Sentry config

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AlertWalutowy
-  class Application < Rails::Application
+  module AlertWalutowy
+    class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -16,4 +16,18 @@ module AlertWalutowy
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  Sentry.init do |config|
+    config.dsn = 'https://ea6b8b1c9f714d1fa3fbe78ad29dca2b@o526871.ingest.sentry.io/5642438'
+    config.breadcrumbs_logger = [:active_support_logger]
+  
+    # To activate performance monitoring, set one of these options.
+    # We recommend adjusting the value in production:
+    config.traces_sample_rate = 0.5
+    # or
+    config.traces_sampler = lambda do |context|
+      true
+    end
+  end
+
 end
