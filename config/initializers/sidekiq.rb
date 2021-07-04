e2f670d27a79d509b32913fcb9ae8ea5925fe0c2
AI-Mozi <bieniek.milosz0@gmail.com>
2021-07-04 14:45:35 +0200

    First version of responsive layout, docker config(not ready, sidekiq conflict)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,3 +3,12 @@ Redis.exists_returns_integer = false
 schedule_file = 'config/schedule.yml'
 
 Sidekiq::Cron::Job.load_from_hash(YAML.load_file(schedule_file)) if File.exist?(schedule_file) && Sidekiq.server?
+
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis:6379' }
+end
